b39ca042aace2e01b903470649b62387c50af924
mrbrunelli <matheus.brunelli@gmail.com>
2020-04-26 00:17:22 -0300

    Funçao para listar usuarios do MySQL e do PgSQL.

--- src/config/fn.php
+++ src/config/fn.php
@@ -1,8 +1,24 @@
 <?php
 
-function listarUsuarios()
+function listarUsuariosMysql()
 {
-    include 'con.php';
+    include 'con-mysql.php';
+    $sql = 'SELECT * FROM usuario';
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $rowCount = $query->rowCount();
+    echo "<h1>$rowCount usuários cadastrados</h1>";
+    foreach ($query as $row) {
+        $id = $row['id'];
+        $nome = $row['nome'];
+        $email = $row['email'];
+        echo "<p>$id - $nome - $email</p>";
+    }
+}
+
+function listarUsuariosPgsql()
+{
+    include 'con-pgsql.php';
     $sql = 'SELECT * FROM usuario';
     $query = $pdo->prepare($sql);
     $query->execute();
